5a2b1cd54e34cdd417f135b2217d5514a51bc6f2
stefanoaragoni <stefanoaragoni13@gmail.com>
2023-07-22 16:48:16 -0600

    LOG IN AND SIGN UP
    Sign Up used XMPP, while Log In uses newer library SleekXMPP. Both functions done.

--- client.py
+++ client.py
@@ -1,9 +1,12 @@
 
+from server import Server, ServerUser
+import threading
+
 class Client():
 
     # This is the constructor for the Client class
     def __init__(self):
-        pass
+        self.server = None
 
     def mostrar_menu_principal(self):
         print("\n----- MENÚ PRINCIPAL -----")
@@ -49,22 +52,64 @@ class Client():
                 print("\n--> Entrada inválida. Por favor, ingrese un número entero.\n")
 
 
+    def solicitar_usuario_contrasena(self):
+        while True:
+            usuario = input("\nIngrese su nombre de usuario: ")
+            contrasena = input("Ingrese su contraseña: ")
+            if usuario != "" and contrasena != "":
+
+                usuario = usuario.split("@")[0] + "@alumchat.xyz"
+
+                return usuario, contrasena
+            else:
+                print("\n--> Usuario y/o contraseña inválidos. Por favor, ingrese valores no vacíos.")
+
 
     def main(self):
         while True:
-            opcion_principal = self.mostrar_menu_principal()
+            opcion_principal = 0
+            success = False
 
-            if opcion_principal == 1:
-                # Registrar una nueva cuenta en el servidor
-                pass
-            elif opcion_principal == 2:
-                # Iniciar sesión con una cuenta
-                pass
-            elif opcion_principal == 3:
-                print("\n--> ¡Hasta luego!")
-                break
-            else:
-                print("\n--> Opción no válida. Por favor, ingrese un número del 1 al 3.")
+            while not success:
+                opcion_principal = self.mostrar_menu_principal()
+
+                if opcion_principal == 1:
+                    # Registrar una nueva cuenta en el servidor
+                    usuario, contrasena = self.solicitar_usuario_contrasena()
+                    serverUser = ServerUser()
+
+                    if serverUser.register(usuario, contrasena):
+                        print("\n--> ¡Registro exitoso!")
+                    else:
+                        print("\n--> ¡Registro fallido!")
+                        opcion_principal = 0
+
+                elif opcion_principal == 2:
+                    # Iniciar sesión con una cuenta
+                    usuario, contrasena = self.solicitar_usuario_contrasena()
+                    self.server = Server(usuario, contrasena)
+
+                    if self.server.login():
+                        print("\n--> ¡Inicio de sesión exitoso!")
+                        success = True
+
+                        xmpp_thread = threading.Thread(target=self.server.process_init)
+                        xmpp_thread.start()
+                        print("XMPP client is now running in the background.")
+                        
+                    else:
+                        print("\n--> ¡Inicio de sesión fallido!")
+                        opcion_principal = 0
+                        
+                        
+                elif opcion_principal == 3:
+                    print("\n--> ¡Hasta luego!")
+                    success = True
+                    exit()
+                else:
+                    print("\n--> Opción no válida. Por favor, ingrese un número del 1 al 3.")
+                    opcion_principal = 0
+
 
             while opcion_principal in [1, 2]:
                 opcion_comunicacion = self.mostrar_menu_comunicacion()
@@ -79,7 +124,16 @@ class Client():
                     # Mostrar detalles de contacto de un usuario
                     pass
                 elif opcion_comunicacion == 4:
-                    # Comunicación 1 a 1 con cualquier usuario/contacto
+                    user_input = input("Type a message to send: ")
+                    recipient_jid = "maldonado20261@alumchat.xyz"
+
+                    if user_input.lower() == "exit":
+                        # Properly disconnect the XMPP client and end the background thread
+                        self.server.disconnect(wait=True)
+                        xmpp_thread.join()
+                        break
+                    else:
+                        self.server.send_xmpp_message(recipient_jid, user_input)
                     pass
                 elif opcion_comunicacion == 5:
                     # Participar en conversaciones grupales
--- server.py
+++ server.py
@@ -1,20 +1,49 @@
 import xmpp
+from sleekxmpp import ClientXMPP
 
-class Server:
+class Server(ClientXMPP):
 
-    # Constructor
-    def __init__(self, server, port):
-        self.server = server
-        self.port = port
+    def __init__(self, jid, password):
+        super().__init__(jid, password)
+        self.add_event_handler("session_start", self.start)
 
-    # Register a new account in the server
+    def start(self, event):
+        self.send_presence()
+        self.get_roster()
+
+    def process_init(self):
+        self.process(threaded=True)
+        
+    def login(self):
+        if self.connect():
+            return True
+        else:
+            return False
+        
+    def message(self, msg):
+        if msg['type'] in ('chat', 'normal'):
+            print(f"Received message from {msg['from']}: {msg['body']}")
+
+    def send_xmpp_message(self, recipient, message):
+        self.send_message(mto=recipient, mbody=message, mtype='chat')
+
+
+class ServerUser():
+
     def register(self, username, password):
-        jid = xmpp.JID(username)
-        self.client = xmpp.Client(jid.getDomain(), debug=[])
-        self.client.connect(server=(self.server, self.port))
+        jid = xmpp.protocol.JID(username)
+        self.xmpp_client = xmpp.Client(jid.getDomain(), debug=[]) 
+
+        if not self.xmpp_client.connect():
+            print("Failed to connect to the XMPP server.")
+            return False
+
+        result = bool(xmpp.features.register(self.xmpp_client, jid.getDomain(), {'username': jid.getNode(), 'password': password}))
+
+        if result:
+            return True
+        else:
+            return False
 
 
-
-
-
-
+ 
